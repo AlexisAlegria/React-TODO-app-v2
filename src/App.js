9250803011c948ba725511f03e89ade464a7593a
AlexisAlegria <alexisalegriav@gmail.com>
2022-03-04 16:42:32 -0300

    fixing form styling

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
-import React, {useState} from 'react';
-import TareaForm from './components/TareaForm';
-import Tarea from './components/Tarea';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import TareaForm from "./components/TareaForm";
+import Tarea from "./components/Tarea";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [listaTareas, setListaTareas] = useState([]);
 
-  const nuevaTarea = (tarea) =>{
+  const nuevaTarea = (tarea) => {
     setListaTareas([tarea, ...listaTareas]);
-  }
+  };
 
-  const borrar = (id) =>{
-    const listaFiltrada = listaTareas.filter((e,index)=> index !== id);
+  const borrar = (id) => {
+    const listaFiltrada = listaTareas.filter((e, index) => index !== id);
     setListaTareas(listaFiltrada);
-  }
-  const actualizarTarea = (id,tarea) => {
-    const listaActualizada = listaTareas.map((e,index)=>{
-      if(index === id){
-        e=tarea;
+  };
+  const actualizarTarea = (id, tarea) => {
+    const listaActualizada = listaTareas.map((e, index) => {
+      if (index === id) {
+        e = tarea;
       }
       return e;
-    })
+    });
     setListaTareas(listaActualizada);
-  }
+  };
 
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
-        <div className="col-6 text-center">
+        <div className="col-10 col-md-6 text-center main-box">
           <h1>todos</h1>
-          <TareaForm 
-            nuevaTarea={nuevaTarea}
-          />
-        <ul className="list-group pb-3">
-          {
-            listaTareas.map((e,index) => 
- 
-                                  <Tarea 
-                                    tarea={e}
-                                    borrar={borrar}
-                                    id={index}
-                                    editar={actualizarTarea}/>)
-          }
+          <TareaForm nuevaTarea={nuevaTarea} />
+          <ul className="list-group pb-3">
+            {listaTareas.map((e, index) => (
+              <Tarea
+                id={index}
+                tarea={e}
+                editar={actualizarTarea}
+                borrar={borrar}
+              />
+            ))}
             <li className="list-group-item counter" id="task-counter">
               {listaTareas.length}{" "}
               {listaTareas.length > 1
-                  ? "items left"
-                  : listaTareas.length === 1
-                  ? "item left"
-                  : "tasks, add a task!"}
+                ? "items left"
+                : listaTareas.length === 1
+                ? "item left"
+                : "tasks, Add a new task!"}
             </li>
           </ul>
         </div>
--- src/components/TareaForm.js
+++ src/components/TareaForm.js
@@ -1,44 +1,56 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import { FaGlasses } from "react-icons/fa";
 
 function TareaForm(props) {
-    const [inputText, setInputText] = useState('')
-    const [validacion, setValidacion] = useState(true)
+    const [inputText, setInputText] = useState("");
+    const [validacion, setValidacion] = useState(true);
 
-    const manejarFormulario = (e) =>{
-        setInputText(e.target.value)
-    }
+    const manejarFormulario = (e) => {
+        setInputText(e.target.value);
+    };
 
-    const submit = (e) =>{
-        e.preventDefault()
-        if(inputText.trim() !== ''){
+    const submit = (e) => {
+        e.preventDefault();
+        if (inputText.trim() !== "") {
             props.nuevaTarea(inputText);
-            setInputText('');
-            setValidacion(true)
+            setInputText("");
+            setValidacion(true);
         } else {
-            setValidacion(false)
+            setValidacion(false);
         }
-    }
+    };
     return (
         <div className="container px-0">
             <div className="row">
-            <form className="form row mb-3 pe-0" onSubmit={submit}>
-                <div className="col">
-                <input
-                    className="form-control" 
-                    value={inputText} 
-                    onChange={manejarFormulario}
-                    placeholder="What's new to be done?"/>
-                </div>
-                <div className="col col-lg-3 px-0">
-                    <button type="submit" className="btn btn-dark">Add task</button>
-                </div>
-            </form>
-            {
-                !validacion &&
-                <div className='validacion'>Add a new task, please</div>
-            }
+                <form className="form d-flex flex-column justify-content-between mb-3" onSubmit={submit}>
+                    <div className="col-12">
+                        <input
+                            className="form-control"
+                            value={inputText}
+                            onChange={manejarFormulario}
+                            placeholder="What's new to be done today?"
+                        />
+                    </div>
+                    <div className="col d-flex">
+                        <button type="submit" className="btn btn-dark">
+                            Add task
+                        </button>
+                    </div>
+                </form>
+                {!validacion && (
+                    <div
+                        className="alert alert-warning alert-dismissible fade show d-flex flex-row justify-content-center align-items-center"
+                        role="alert"
+                    >
+                        <div className="glasses">
+                            <FaGlasses />
+                        </div>
+                        <div>Add a new task, please!</div>
+                        {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+                    </div>
+                )}
             </div>
         </div>
-    )
+    );
 }
-export default TareaForm
+export default TareaForm;
